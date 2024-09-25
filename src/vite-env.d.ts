a70abd40eae9cfe98198afe6110ddd914c0072e6
t2tc <61874231+t2tc@users.noreply.github.com>
2024-09-25 23:40:57 +0800

    add a nifty support for web components typing for react. react sucks.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+type StringWithDash = `${string}-${string}`;
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        [key: StringWithDash]: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+            class?: string;
+        };
+    }
+}
